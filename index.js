ab691291c7233006d846352fab9389b966249d30
codecamp.official.thailand <codecamp.official.thailand@gmail.com>
2020-11-04 15:08:41 +0700

    finish friend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 const commentRoutes = require('./routes/comment');
+const friendRoutes = require("./routes/friend");
 
 require("./config/passport");
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 app.use("/comments", commentRoutes);
+app.use("/friends", friendRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
